c9dd1090ee354f8018a345408d3ec431bb23a70a
Shylin Denis <tawnship406@gmail.com>
2024-11-23 17:57:58 +0200

    Зміна розмірив в li

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import './js/faq';
 import 'accordion-js/dist/accordion.min.css';
-import { initSlider } from './js/reviews';
+// import { initSlider } from './js/reviews';
 
-document.addEventListener('DOMContentLoaded', () => {
-  initSlider();
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//   initSlider();
+// });
 
 import './js/myprojects';
