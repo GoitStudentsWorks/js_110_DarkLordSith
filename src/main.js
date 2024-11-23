b5d8846fdfc074892ed46b3816d7403d65d1a2e0
vanya-n23 <vanya.neresnitsia@gmail.com>
2024-11-23 21:23:40 +0300

    uncoment

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import './js/faq';
-import 'accordion-js/dist/accordion.min.css';
-// import { initSlider } from './js/reviews';
+import { initSlider } from './js/reviews';
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   initSlider();
-// });
+document.addEventListener('DOMContentLoaded', () => {
+  initSlider();
+});
 
 import './js/myprojects';
