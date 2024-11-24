5bf354f504ba5371738286842b051b20925cc2c0
Shylin Denis <tawnship406@gmail.com>
2024-11-24 14:58:50 +0200

    main js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,3 @@
 import './js/faq';
-import 'accordion-js/dist/accordion.min.css';
-import { initSlider } from './js/reviews';
-
-document.addEventListener('DOMContentLoaded', () => {
-  initSlider();
-});
-
 import './js/reviews';
-
 import './js/myprojects';
